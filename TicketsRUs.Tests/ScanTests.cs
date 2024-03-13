b06c227bd12b0bc29244cddb174b7510f8a1ee01
Luris Solis <luris.solis@students.snow.edu>
2024-03-13 12:07:08 -0600

    green unit test

--- TicketsRUs.Tests/ScanTests.cs
+++ TicketsRUs.Tests/ScanTests.cs
@@ -25,7 +25,7 @@ public class ScanTests
         Ticket wrongTicket = new Ticket()
         {
             Id = 0,
-            EventId = 0,
+            EventId = 1,
             Scanned = false,
             Identifier = "123456789"
         };
