a96e1e8a113b1c42d813f7ae710cb90521910aed
Luris Solis <luris.solis@students.snow.edu>
2024-03-18 13:06:15 -0600

    Test Pass?

--- TicketsRUs.WebApp/Program.cs
+++ TicketsRUs.WebApp/Program.cs
@@ -4,6 +4,14 @@ using TicketsRUs.WebApp.Components;
 using TicketsRUs.WebApp.Services;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.EntityFrameworkCore;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using System.Diagnostics.Metrics;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,6 +28,35 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContextFactory<PostgresContext>(options => options.UseNpgsql("Name=db"));
+
+
+const string serviceName = "luris";
+
+builder.Services.AddOpenTelemetry()
+    .ConfigureResource(resource => resource.AddService(serviceName))
+    .WithTracing(tracing => tracing
+    .AddAspNetCoreInstrumentation()
+    .AddSource(DiagnosticsTrace.SourceName)
+    .AddSource(DiagnosticsTrace.SourceName2)
+    .AddConsoleExporter()
+    .AddOtlpExporter(o =>
+    {
+        o.Endpoint = new Uri("http://otel-collector:4317/");
+    })
+    )
+    .WithMetrics(metrics =>
+         metrics
+        .AddAspNetCoreInstrumentation()
+        .AddMeter(Meters.myhomeworkmeter.Name)
+        .AddConsoleExporter()
+        .AddOtlpExporter(o =>
+        {
+            o.Endpoint = new Uri("http://otel-collector:4317/");
+        }
+        )
+    );
+
+
 
 var app = builder.Build();
 
