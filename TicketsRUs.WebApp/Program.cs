22464b1835d611bf2f82795d15f64075c335e326
luris.solis <luris.solis@students.snoe.edu>
2024-03-20 23:45:05 -0600

    ajskj

--- TicketsRUs.WebApp/Program.cs
+++ TicketsRUs.WebApp/Program.cs
@@ -92,6 +92,8 @@ var handler = app.Services.GetRequiredService<ExampleHandler>();
 app.MapGet("/log", ()=>handler.HandleRequest());
 app.MapGet("/log2", ()=>handler.HandleRequest());
 app.MapGet("/log3", ()=>handler.HandleRequest());
+app.MapGet("/log4", ()=>handler.HandleRequest());
+app.MapGet("/log5", ()=>handler.HandleRequest());
 
 app.MapGet("/healthCheck", () =>
 {
